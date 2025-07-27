a781f12fdb24702785968e92d07b8577cf921012
qinanagao@gmail.com <qinanagao@gmail.com>
2025-07-27 13:49:02 +0100

    replace about with landing page

---
+++ app/landing.jsx
@@ -0,0 +1,38 @@
+// app/landing.jsx
+import { styles } from "@/assets/styles/landing.styles";
+import { useRouter } from "expo-router";
+import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
+
+export default function Landing() {
+  const router = useRouter();
+
+  const handleGetStarted = () => {
+    router.push("/(auth)/sign-in");
+  };
+
+  return (
+    <ImageBackground
+      source={require("@/assets/images/landing.png")}
+      style={styles.background}
+      resizeMode="cover"
+    >
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <View>
+            <Text style={styles.title}>Welcome to Walletry</Text>
+            <Text style={styles.subtitle}>
+              Track your expenses easily and efficiently
+            </Text>
+          </View>
+
+          <TouchableOpacity
+            style={styles.getStartedButton}
+            onPress={handleGetStarted}
+          >
+            <Text style={styles.getStartedText}>Get Started</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ImageBackground>
+  );
+}
