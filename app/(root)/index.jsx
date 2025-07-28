a9739be40cb695062ea587beacdc00321c360636
qinanagao@gmail.com <qinanagao@gmail.com>
2025-07-28 14:31:49 +0100

    create home page with summary and list

--- app/(root)/index.jsx
+++ app/(root)/index.jsx
@@ -1,25 +1,110 @@
-import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { useUser } from "@clerk/clerk-expo";
+
+import { Ionicons } from "@expo/vector-icons";
+import { Image } from "expo-image";
+import { useRouter } from "expo-router";
+import { useEffect, useState } from "react";
+import {
+  Alert,
+  FlatList,
+  RefreshControl,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { styles } from "../../assets/styles/home.styles";
+import BalanceCard from "../../components/BalanceCard";
+import NoTransactionsUI from "../../components/NoTransactionsUI";
+import PageLoader from "../../components/PageLoader";
 import { SignOutButton } from "../../components/SignOutButton";
+import { TransactionItem } from "../../components/TransactionItem";
+import { useTransactions } from "../../hooks/useTransactions";
 
 export default function Page() {
   const { user } = useUser();
+  const router = useRouter();
+
+  const [refreshing, setRefreshing] = useState(false);
+
+  const { transactions, isLoading, summary, loadData, deleteTransaction } =
+    useTransactions(user.id);
+
+  useEffect(() => {
+    if (user?.id) {
+      loadData();
+    }
+  }, [user?.id, loadData]);
+
+  function handleDelete(id) {
+    Alert.alert("Delete", "Are you sure you want to delete this transaction?", [
+      { text: "Cancel", style: "cancel" },
+      {
+        text: "Delete",
+        style: "destructive",
+        onPress: () => deleteTransaction(id),
+      },
+    ]);
+  }
+
+  async function handleRefresh() {
+    setRefreshing(true);
+    await loadData();
+    setRefreshing(false);
+  }
+  if (isLoading && !refreshing) return <PageLoader />;
 
   return (
-    <View>
-      <SignedIn>
-        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-        <SignOutButton />
-      </SignedIn>
-      <SignedOut>
-        <Link href="/(auth)/sign-in">
-          <Text>Sign in</Text>
-        </Link>
-        <Link href="/(auth)/sign-up">
-          <Text>Sign up</Text>
-        </Link>
-      </SignedOut>
+    <View style={styles.container}>
+      <View style={styles.content}>
+        {/* HEADER */}
+        <View style={styles.header}>
+          {/* left */}
+          <View style={styles.headerLeft}>
+            <Image
+              source={require("../../assets/images/logo.png")}
+              style={styles.headerLogo}
+            />
+            <View style={styles.welcomeContainer}>
+              <Text style={styles.welcomeText}>Weclome,</Text>
+              <Text style={styles.usernameText}>
+                {user?.emailAddresses[0]?.emailAddress.split("@")[0]}
+              </Text>
+            </View>
+          </View>
+          {/* right */}
+          <View style={styles.headerRight}>
+            <TouchableOpacity
+              style={styles.addButton}
+              onPress={() => router.push("/create")}
+            >
+              <Ionicons name="add" size={20} color="#fff" />
+              <Text style={styles.addButtonText}>Add</Text>
+            </TouchableOpacity>
+            <SignOutButton />
+          </View>
+        </View>
+
+        {/* Balance card */}
+        <BalanceCard summary={summary} />
+        <View style={styles.transactionsHeaderContainer}>
+          <Text style={styles.sectionTitle}>Recent Transactions</Text>
+        </View>
+      </View>
+
+      <FlatList
+        style={styles.transactionsList}
+        contentContainerStyle={styles.transactionsListContent}
+        data={transactions}
+        // data={[]}
+        renderItem={({ item }) => (
+          <TransactionItem item={item} onDelete={handleDelete} />
+        )}
+        ListEmptyComponent={<NoTransactionsUI />}
+        showsVerticalScrollIndicator={false}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />
+        }
+      />
     </View>
   );
 }
