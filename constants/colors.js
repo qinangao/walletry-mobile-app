59e4c849885c1e0b6403dc57533537511b1cb52c
qinanagao@gmail.com <qinanagao@gmail.com>
2025-07-28 16:50:25 +0100

    update colors

--- constants/colors.js
+++ constants/colors.js
@@ -1,36 +1,20 @@
-const coffeeTheme = {
-  primary: "#94DFFE",
+const defaultTheme = {
+  primary: "#4AB8E6",
   background: "#fbfbfb",
   text: "#282828",
   border: "#D8DCDF",
   white: "#FFFFFF",
   textLight: "#708090",
   expense: "#E66D47",
-  income: "#C4D588",
+  income: "#B1C675",
   card: "#FFFFFF",
   navy: "#333743",
   shadow: "#282828",
   boxShadow: "0px 2px 4px rgba(122, 121, 121, 0.1)",
 };
 
-// const oceanTheme = {
-//   primary: "#0277BD",
-//   background: "#E1F5FE",
-//   text: "#01579B",
-//   border: "#B3E5FC",
-//   white: "#FFFFFF",
-//   textLight: "#4FC3F7",
-//   expense: "#EF5350",
-//   income: "#26A69A",
-//   card: "#FFFFFF",
-//   shadow: "#000000",
-//   boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
-// };
-
 export const THEMES = {
-  coffee: coffeeTheme,
-  // ocean: oceanTheme,
+  default: defaultTheme,
 };
 
-// 👇 change this to switch theme
-export const COLORS = THEMES.coffee;
+export const COLORS = THEMES.default;
