b15917940fe3d4266d62640bfdde5be29e488326
qinanagao@gmail.com <qinanagao@gmail.com>
2025-07-28 16:52:44 +0100

    remove api url

--- hooks/useTransactions.js
+++ hooks/useTransactions.js
@@ -1,8 +1,6 @@
 import { useCallback, useState } from "react";
 import { Alert } from "react-native";
-
-const API_URL = "https://expense-tracker-backend-pdug.onrender.com/api";
-// const API_URL = "http://localhost:5001/api";
+import { API_URL } from "../constants/api";
 
 export function useTransactions(userId) {
   const [transactions, setTransactions] = useState([]);
